5f811fdc1d0ef741ac239ec8008063cfb9642b70
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-13 16:27:09 +0900

    "2022/03/13 깃테스트 두번째 "

---
+++ src/main/java/GitTest2.java
@@ -0,0 +1,10 @@
+public class GitTest2 {
+
+    public static void main(String[] args){
+
+        String a = "깃에 두번째로 올라갈 파일입니다 ";
+        String b = "2022-03-13 04:23";
+
+        System.out.println(a+b);
+    }
+}

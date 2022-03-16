2f82362163ffa71cf101d0fa5c8963ecbfe047df
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-16 09:50:26 +0900

    "Ex03_03.java"

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,15 @@
+public class Exam01 {
+
+    public static void main(String[] args) {
+        System.out.println("김지태");
+        System.out.println("경기도 고양시 일산동구 강촌로 114");
+        System.out.println("taetaetae96@anver.com");
+        System.out.println("010-9508-7881");
+        System.out.println("1996.08.22");
+        System.out.println(27);
+
+
+
+    }
+
+}

490a5fdc5a27e2cb9d7703906cc2aaacc4f4392c
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-16 09:55:52 +0900

    "Practice2"

---
+++ src/main/java/Ex03_02.java
@@ -0,0 +1,8 @@
+public class Ex03_02 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.println();
+        System.out.printf("%d", 100+100);
+
+    }
+}
---
+++ src/main/java/Homework.java
@@ -0,0 +1,7 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println((3.14));
+        System.out.println("Hello\nWorld");
+    }
+}

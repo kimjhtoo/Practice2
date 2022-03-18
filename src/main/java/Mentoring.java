fcf3ca2a5efbaa2df44e1fc41843145fc3a032ce
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-18 15:21:14 +0900

    "Mentoring"

---
+++ src/main/java/Mentoring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mentoring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       int num1 = sc.nextInt();
+       int num2 = sc.nextInt();
+       String a ="한줄표현";
+
+        System.out.println("정수 = "+(num1 +num2));
+    }
+
+    }

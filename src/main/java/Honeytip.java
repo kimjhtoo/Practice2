883333ef84f1b867d53225e02fcceeb431032e48
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-20 13:00:17 +0900

    "Honeytip"

---
+++ src/main/java/Honeytip.java
@@ -0,0 +1,15 @@
+public class Honeytip {
+    public static void main(String[] args) {
+        System.out.println("안녕 자바!");
+        System.out.println("김지태");
+        System.out.println("치킨은 살 안쪄요.");
+        System.out.println("살은 내가쪄요...");
+
+        System.out.println(4.3+.2);
+
+
+
+
+    }
+
+}

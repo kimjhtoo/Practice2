83454d4fd35cfaf8b68f6d7966c60dae2ff4896d
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-26 20:30:01 +0900

    "Study"

---
+++ src/main/java/study.java
@@ -0,0 +1,11 @@
+public class study {
+    public static void main(String[] args) {
+
+        float a;
+        int b;
+        float c;
+        a = 123.45f;
+        b = (int)a;
+        c = b;
+}
+}

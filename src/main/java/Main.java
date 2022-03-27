acdcca457c68f4763c2a4dc3984d9296c1307aa7
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-27 15:04:50 +0900

    "2022.03.27 Study"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        char var1 = '지';
+        // char는 오로지 한글자만 가능 , ''(작은 따옴표)만 사용
+        String var2 = "지태";
+        // String은 한글자 이상 가능 , ""(큰 따옴표)만 사용
+        int var3 = 2;
+        // int는 정수
+        double var4 = 3.14;
+        // double은 float 보다 큰 소수 선언 가능
+        float var5 = 3.14f;
+        // float는 소수점 선언후 뒤에 f를 무조건 붙여줘야함
+        boolean isStart = false; // true or false를 입력하여 조건식에서 많이 사용함.
 
-        int y = sc.nextInt();
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(isStart);
 
 
-        if(y%4==0) {
-            if (y % 400 == 0) System.out.println("1");
-            else if (y % 100 == 0) System.out.println("0");
-            else System.out.println("1");
-        }
-        else System.out.println("0");
-}
+
     }
+}

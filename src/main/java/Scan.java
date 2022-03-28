97e4fabef97220d463659afc36c23039528e8aff
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-28 15:02:09 +0900

    "2022.03.28 Scanner - 커밋"

--- src/main/java/Scan.java
+++ src/main/java/Scan.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 // String은 문자열, 크기제한없음
-// int는 작은정수 , 4 바이트
-// long은 큰정수, 8 바이트
+// int는 작은 정수 , 4 바이트
+// long은 큰 정수, 8 바이트
 // float은 작은 실수 , 4바이트
 // double은 큰 실수 , 8바이트
 

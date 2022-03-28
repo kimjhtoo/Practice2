a2ed1ebee4cf730522bde57b292425a269316151
kimjhtoo <kimjhtoo96@gmail.com>
2022-03-28 12:18:39 +0900

    "2022.03.28 Scanner 공부"

---
+++ src/main/java/Scan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// String은 문자열, 크기제한없음
+// int는 작은정수 , 4 바이트
+// long은 큰정수, 8 바이트
+// float은 작은 실수 , 4바이트
+// double은 큰 실수 , 8바이트
+
+
+public class Scan {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+        String str2 = scan.nextLine();
+
+        System.out.println(str);
+        System.out.println(str2);
+    }
+}

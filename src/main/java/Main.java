65183866afe55a180b3e33d24c7cda8b89be8937
kimjhtoo <kimjhtoo96@gmail.com>
2022-04-04 08:40:31 +0900

    "2022.04.04 백준 구구단 문제풀이 커밋"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        int Years = sc.nextInt();
-        System.out.println( Years -543);
+        Scanner s= new Scanner(System.in);
+
+        int i = s.nextInt();
+        int j =1;
+
+        System.out.println(i);
+
+        for( j=1 ; j<10 ; j++){
+            System.out.println(i + "x" + j + "=" + i*j);
+        }
+
     }
+
+
 }

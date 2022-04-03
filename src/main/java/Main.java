9b630c4dad041a53bbaee32d9cccece720e631c5
kimjhtoo <kimjhtoo96@gmail.com>
2022-04-04 08:42:10 +0900

    "2022.04.04 백준 구구단 문제풀이 커밋"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
 
-        int i = s.nextInt();
-        int j =1;
-
-        System.out.println(i);
-
-        for( j=1 ; j<10 ; j++){
-            System.out.println(i + "x" + j + "=" + i*j);
+        for (int i = 1; i <= 9; ++i) {
+            System.out.println(n + " * " + i + " = " + (n * i));
         }
-
     }
-
-
 }
